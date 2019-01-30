c87a01f56e6a3bd39b86f4370c3ba3e329356180
Unknown <ruofu.sun@gmail.com>
2019-01-30 15:22:03 -0500

    Basic hitbox and hurtbox works G--projectile attack H--left arm attack J--right arm attack

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -7,7 +7,7 @@ public class Fighter : MonoBehaviour
     private CharacterController controller;
     private Vector3 moveVector;
     private float verticalVelocity;
-
+    public Collider[] attackHitboxes;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +16,25 @@ public class Fighter : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   //projectile
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            LaunchAttack(attackHitboxes[0]);
+        }
+        //arm1
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            LaunchAttack(attackHitboxes[1]);
+        }
+        //arm2
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            LaunchAttack(attackHitboxes[2]);
+        }
+
+
         //handle jump 
-        if(controller.isGrounded)
+        if (controller.isGrounded)
         {
             verticalVelocity = -1;
             if(Input.GetKeyDown(KeyCode.Space))
@@ -35,4 +51,20 @@ public class Fighter : MonoBehaviour
         moveVector.y = verticalVelocity;
         controller.Move(moveVector * Time.deltaTime);
     }
+    void LaunchAttack(Collider col)
+    {   // or can use an overlapsphere here which is cheaper but may require multiple spheres
+        //test against colliders only in the Hitbox layer(now only contain the head and the torso)
+        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, transform.rotation, LayerMask.GetMask("Hitbox")) ;
+        foreach(Collider c in cols)
+        {
+            //check if the collider we hit is ourself 
+            if (c.transform.parent.parent == transform)
+                continue;
+            //only print the collider from enemy  
+            Debug.Log(c.name);
+        }
+    }
+
+
+
 }
