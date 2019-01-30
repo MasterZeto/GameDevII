f12815298f2b0244866a28a97a35bdaf233ca41d
Unknown <richas3@rpi.edu>
2019-01-30 14:23:47 -0500

    Added beginning of pause and unpause command

---
+++ Assets/Scripts/Test_Scripts/Command.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Add this stuff to the main command pattern script later, some commands are just here for testing
+
+namespace CommandPattern
+{
+    public abstract class Command{
+        public abstract void Execute(Command command);
+    }
+    
+    public class PauseGame : Command{
+        public override void Execute(Command command){
+            Time.timeScale=0.0f;
+        }
+    }
+    public class UnpauseGame : Command{
+        public override void Execute(Command command){
+            Time.timeScale=1.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Test_Scripts/dummyPauseScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CommandPattern
+{
+    public class dummyPauseScript : MonoBehaviour
+    {
+        // bool pause=false;
+        private Command Pause, Unpause;
+        bool pause = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Pause=new PauseGame();
+            Unpause= new UnpauseGame();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.P)){
+                if(!pause){
+                    Pause.Execute(Pause);
+                    pause=true;
+                }
+                else{
+                    Unpause.Execute(Unpause);
+                    pause=false;
+                }
+                
+            }
+            /* if(Input.GetKeyDown(KeyCode.P)&&!pause){
+            Time.timeScale=0;
+            //UI shit here... change this to command pattern too
+            }
+            else if(Input.GetKeyDown(KeyCode.P)&&pause){
+                Time.timeScale=1;
+            }*/
+        }
+    }
+}
