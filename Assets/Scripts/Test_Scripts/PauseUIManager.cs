a8f6d27e8cb611e8473f79c6e757698307d09c1a
Unknown <richas3@rpi.edu>
2019-02-06 15:28:29 -0500

    No actual changes, just changed some formatting on the scripts

--- Assets/Scripts/Test_Scripts/PauseUIManager.cs
+++ Assets/Scripts/Test_Scripts/PauseUIManager.cs
@@ -3,112 +3,111 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 namespace CommandPattern
-{
-    
+{  
 
-public class PauseUIManager : MonoBehaviour
-{
-    public Button bttnPrefab;
-    public int maxQueued = 5;
-    public Canvas UIparent;
-    //change this to change dynamically, ex. with the customizations once they are implemented, might want it to be a list
-    public List<Button> bttns;
-    //To Do: change this to a dictionary, and have addToQueue in the PauseScript add different commands based on the value
-    private Dictionary<Button, int> queueButtons;
-    private Dictionary<Button, RectTransform> buttonLoc;
-    private Vector3 currentPos, startPos;
-    private Transform parent;
-    //change dummyPauseScript to just PauseScript later once it is more fleshed out
-    public GameObject pauseManager;
-    private DummyPauseScript PauseScript;
-    private int currentIndex=0;
-    // Start is called before the first frame update
-    void Start()
+    public class PauseUIManager : MonoBehaviour
     {
-        queueButtons=new Dictionary<Button, int>();
-        buttonLoc=new Dictionary<Button, RectTransform>();
-        PauseScript=pauseManager.GetComponent<DummyPauseScript>();
-        for(int i = 0; i<PauseScript.numOfPossComs; i++){
-           Button newBttn=Instantiate(bttnPrefab);
-           RectTransform rect=newBttn.GetComponent<RectTransform>();
-           rect.transform.SetParent(UIparent.transform);
-           startPos.x=Screen.width/2-PauseScript.numOfPossComs/2*rect.sizeDelta.x+i*rect.sizeDelta.x;
-           startPos.y=4*rect.sizeDelta.y;
-           startPos.z=0;
-           rect.transform.position=startPos;
-           newBttn.GetComponentInChildren<Text>().text="Action"+(i+1);
-           bttns.Add(newBttn);
-        }
-        foreach (Button bttn in bttns)
+        public Button bttnPrefab;
+        public int maxQueued = 5;
+        public Canvas UIparent;
+        //change this to change dynamically, ex. with the customizations once they are implemented, might want it to be a list
+        public List<Button> bttns;
+        //To Do: change this to a dictionary, and have addToQueue in the PauseScript add different commands based on the value
+        private Dictionary<Button, int> queueButtons;
+        private Dictionary<Button, RectTransform> buttonLoc;
+        private Vector3 currentPos, startPos;
+        private Transform parent;
+        //change dummyPauseScript to just PauseScript later once it is more fleshed out
+        public GameObject pauseManager;
+        private DummyPauseScript PauseScript;
+        private int currentIndex=0;
+        // Start is called before the first frame update
+        void Start()
         {
-            bttn.onClick.AddListener(() => AddToQueue(bttn));
+            queueButtons = new Dictionary<Button, int>();
+            buttonLoc = new Dictionary<Button, RectTransform>();
+            PauseScript = pauseManager.GetComponent<DummyPauseScript>();
+            for(int i = 0; i < PauseScript.numOfPossComs; i++){
+                Button newBttn = Instantiate(bttnPrefab);
+                RectTransform rect = newBttn.GetComponent<RectTransform>();
+                rect.transform.SetParent(UIparent.transform);
+                startPos.x = Screen.width/2-PauseScript.numOfPossComs/2*rect.sizeDelta.x+i*rect.sizeDelta.x;
+                startPos.y = 4*rect.sizeDelta.y;
+                startPos.z = 0;
+                rect.transform.position=startPos;
+                newBttn.GetComponentInChildren<Text>().text = "Action" + (i + 1);
+                bttns.Add(newBttn);
+            }
+            foreach (Button bttn in bttns)
+            {
+                bttn.onClick.AddListener(() => AddToQueue(bttn));
+            }
+            startPos.x = Screen.width/2-maxQueued/2*bttns[0].GetComponent<RectTransform>().sizeDelta.x;
+            startPos.y = 2*bttns[0].GetComponent<RectTransform>().sizeDelta.y;
+            startPos.z = 0;
+            currentPos = startPos;
+            parent = bttns[0].GetComponent<RectTransform>().transform.parent;
+            Hide();
         }
-        startPos.x=Screen.width/2-maxQueued/2*bttns[0].GetComponent<RectTransform>().sizeDelta.x;
-        startPos.y=2*bttns[0].GetComponent<RectTransform>().sizeDelta.y;
-        startPos.z=0;
-        currentPos=startPos;
-        parent=bttns[0].GetComponent<RectTransform>().transform.parent;
-        Hide();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void AddToQueue(Button bttn){
-        if(PauseScript.pauseQueue.Count<maxQueued){
-            Button newBttn=Instantiate(bttn);
-            RectTransform rect=newBttn.GetComponent<RectTransform>();
-            queueButtons.Add(newBttn,currentIndex);
-            buttonLoc.Add(newBttn,rect);
-            currentIndex++;
-            rect.transform.SetParent(parent);
-            rect.transform.position=currentPos;
-            currentPos.x+=rect.sizeDelta.x;
-            newBttn.onClick.AddListener(() => DeleteButton(newBttn));
-            int index=bttns.IndexOf(bttn);
-            PauseScript.addToQueue(index);
+        // Update is called once per frame
+        void Update()
+        {
+            
         }
-    }
-    void DeleteButton(Button bttn){
-        //To Do: have spawned buttons after the removed one slide down, and update currentPos.x
-        //To Do: find the command in the pauseQueue and remove it
-        PauseScript.pauseQueue.RemoveAt(queueButtons[bttn]);
-        int index=queueButtons[bttn];
-        currentIndex--;
-        queueButtons.Remove(bttn);
-        
-        List<Button> tempBttns=new List<Button>(queueButtons.Keys);
-        foreach(Button otherBttn in tempBttns){
-            if(queueButtons[otherBttn]>index){
-               queueButtons[otherBttn]--;
-                Vector3 newLoc = buttonLoc[otherBttn].transform.position;
-                newLoc.x -= buttonLoc[bttn].sizeDelta.x;
-                buttonLoc[otherBttn].transform.position=newLoc;
+        void AddToQueue(Button bttn){
+            if(PauseScript.pauseQueue.Count<maxQueued){
+                Button newBttn = Instantiate(bttn);
+                RectTransform rect = newBttn.GetComponent<RectTransform>();
+                queueButtons.Add(newBttn,currentIndex);
+                buttonLoc.Add(newBttn,rect);
+                currentIndex++;
+                rect.transform.SetParent(parent);
+                rect.transform.position = currentPos;
+                currentPos.x += rect.sizeDelta.x;
+                newBttn.onClick.AddListener(() => DeleteButton(newBttn));
+                int index=bttns.IndexOf(bttn);
+                PauseScript.addToQueue(index);
             }
         }
-        currentPos.x-=buttonLoc[bttn].sizeDelta.x;
-        Destroy(bttn.gameObject);
-    }
-    public void SetUp(){
-        currentIndex=0;
-        queueButtons.Clear();
-        currentPos=startPos;
-        foreach(Button bttn in bttns){
-            bttn.gameObject.SetActive(true);
+        void DeleteButton(Button bttn){
+            //To Do: have spawned buttons after the removed one slide down, and update currentPos.x
+            //To Do: find the command in the pauseQueue and remove it
+            PauseScript.pauseQueue.RemoveAt(queueButtons[bttn]);
+            int index = queueButtons[bttn];
+            currentIndex--;
+            queueButtons.Remove(bttn);
+        
+            List<Button> tempBttns = new List<Button>(queueButtons.Keys);
+            foreach(Button otherBttn in tempBttns){
+                if(queueButtons[otherBttn]>index){
+                    queueButtons[otherBttn]--;
+                    Vector3 newLoc = buttonLoc[otherBttn].transform.position;
+                    newLoc.x -= buttonLoc[bttn].sizeDelta.x;
+                    buttonLoc[otherBttn].transform.position=newLoc;
+                }
+            }
+            currentPos.x -= buttonLoc[bttn].sizeDelta.x;
+            Destroy(bttn.gameObject);
         }
-    }
-    public void Hide(){
-        foreach(Button bttn in bttns){
-            bttn.gameObject.SetActive(false);
+        public void SetUp(){
+            currentIndex = 0;
+            queueButtons.Clear();
+            currentPos = startPos;
+            foreach(Button bttn in bttns){
+                bttn.gameObject.SetActive(true);
+            }
         }
-        foreach(var bttn in queueButtons){
-            //To do: change so that it doesn't destroy itself until the action is done
-            if(bttn.Key.gameObject!=null){
-                Destroy(bttn.Key.gameObject);
+        public void Hide(){
+            foreach(Button bttn in bttns){
+                bttn.gameObject.SetActive(false);
+            }
+            foreach(var bttn in queueButtons){
+                //To do: change so that it doesn't destroy itself until the action is done
+                if(bttn.Key.gameObject!=null){
+                    Destroy(bttn.Key.gameObject);
+                }
             }
         }
     }
-}
 }
--- Assets/Scripts/Test_Scripts/dummyPauseScript.cs
+++ Assets/Scripts/Test_Scripts/dummyPauseScript.cs
@@ -6,7 +6,6 @@ namespace CommandPattern
 {
     public class DummyPauseScript : MonoBehaviour
     {
-        // bool pause=false;
         //possible coms should store the possible list of commands that can currently be executed
         private List<Command> possibleComs;
         public int numOfPossComs = 3;
@@ -17,34 +16,33 @@ namespace CommandPattern
         public PauseUIManager UIManager;
         private float deltaTime, lastFrozenTime;
         //will change this once timing is added to commands or w/e
-        private bool waiting=false;
+        private bool waiting = false;
         // Start is called before the first frame update
         void Start()
         {
-            UIManager=pauseManager.GetComponent<PauseUIManager>();
-            possibleComs= new List<Command>();
+            UIManager = pauseManager.GetComponent<PauseUIManager>();
+            possibleComs = new List<Command>();
             Pause = new PauseGame();
             Unpause = new UnpauseGame();
             pauseQueue = new List<Command>();
             possibleComs.Add(new dummyCom());
             possibleComs.Add(new dummyCom2());
             possibleComs.Add(new dummyCom3());
-            camMoveForward=new MoveForwardUnscaled();
-            camMoveBack=new MoveBackUnscaled();
-            camMoveRight=new MoveRightUnscaled();
-            camMoveLeft=new MoveLeftUnscaled();
-            lastFrozenTime=Time.unscaledTime;
+            camMoveForward = new MoveForwardUnscaled();
+            camMoveBack = new MoveBackUnscaled();
+            camMoveRight = new MoveRightUnscaled();
+            camMoveLeft = new MoveLeftUnscaled();
+            lastFrozenTime = Time.unscaledTime;
         }
 
         // Update is called once per frame
         void Update()
         {
             if(Input.GetKeyDown(KeyCode.P)){
-                //To Do: try to add all the UI and other stuff to the command rather than being a part of this script
                 //Also change the input thing and move it to the InputHandler 
-                if(!pause&&!waiting){
+                if(!pause && !waiting){
                     Pause.Execute(Pause);
-                    lastFrozenTime=Time.unscaledTime;
+                    lastFrozenTime = Time.unscaledTime;
                     UIManager.SetUp();
                     pause = true;
                     pauseQueue.Clear();
@@ -58,20 +56,20 @@ namespace CommandPattern
                 }
                 
             }
-            deltaTime=Time.unscaledTime-lastFrozenTime;
-            lastFrozenTime=Time.unscaledTime;
+            deltaTime = Time.unscaledTime - lastFrozenTime;
+            lastFrozenTime = Time.unscaledTime;
             if(pause){
                 //have to use axis raw because freezing time stops acceleration, meaning the acceleration of axis too
-                if(Input.GetAxisRaw("Vertical")==1){
+                if(Input.GetAxisRaw("Vertical") == 1){
                     camMoveForward.Move(camMoveForward, deltaTime, Camera.main.transform);
                 }
-                if(Input.GetAxisRaw("Vertical")==-1){
+                if(Input.GetAxisRaw("Vertical") == -1){
                 camMoveBack.Move(camMoveBack, deltaTime, Camera.main.transform);
                 }
-                if(Input.GetAxisRaw("Horizontal")==1){
+                if(Input.GetAxisRaw("Horizontal") == 1){
                     camMoveRight.Move(camMoveRight, deltaTime, Camera.main.transform);
                 }
-                if(Input.GetAxisRaw("Horizontal")==-1){
+                if(Input.GetAxisRaw("Horizontal") == -1){
                 camMoveLeft.Move(camMoveLeft, deltaTime, Camera.main.transform);
                 }
             }
