3efdd0ff62c6b78ebbbb287b49d743019f066314
Unknown <ruofu.sun@gmail.com>
2019-02-08 11:02:34 -0500

    Change the format of script to command pattern

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command:MonoBehaviour
+{
+    //child classes have to override the function
+    public abstract void Execute();
+    //child classes have the option to override the function
+    public virtual void LaunchAttack(Collider col) {
+        // or can use an overlapsphere here which is cheaper but may require multiple spheres
+        //test against colliders only in the Hitbox layer(now only contain the head and the torso)
+        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, transform.rotation, LayerMask.GetMask("Hitbox"));
+        foreach (Collider c in cols)
+        {
+            //check if the collider we hit is ourself 
+            if (c.transform.root == transform)
+                continue;
+            //only print the collider from enemy  
+            Debug.Log(c.name);
+        }
+
+
+    }
+}
+
+//child classes
+public class ArmAttack: Command
+{
+
+    public override void Execute()
+    {
+        
+    }
+
+}
+
+public class ProjectileAttack:Command
+{
+    public override void Execute()
+    {
+       
+    }
+}
+
+public class DoNothing:Command
+{
+    public override void Execute()
+    {
+
+    }
+}
