4b4831ae12ff37c9170ff72123bd040bfbfdf862
Unknown <ruofu.sun@gmail.com>
2019-02-09 17:13:36 -0500

    Fixed the null reference error

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -2,52 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Command:MonoBehaviour
+namespace CommandPatternNew
 {
-    //child classes have to override the function
-    public abstract void Execute(Collider col);
-    //child classes have the option to override the function
-    public virtual void LaunchAttack(Collider col) {
-        // or can use an overlapsphere here which is cheaper but may require multiple spheres
-        //test against colliders only in the Hitbox layer(now only contain the head and the torso)
-        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, transform.rotation, LayerMask.GetMask("Hitbox"));
-        foreach (Collider c in cols)
+    public abstract class Command 
+    {
+       
+        //child classes have to override the function
+        public abstract void Execute(Collider col, Transform transform);
+        //child classes have the option to override the function
+        public virtual void LaunchAttack(Collider col, Transform transform)
         {
-            //check if the collider we hit is ourself 
-            if (c.transform.root == transform)
-                continue;
-            //only print the collider from enemy  
-            Debug.Log(c.name);
+
+            if(col==null)
+            {
+                Debug.Log("no col");
+            }
+            // or can use an overlapsphere here which is cheaper but may require multiple spheres
+            //test against colliders only in the Hitbox layer(now only contain the head and the torso)
+            Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, transform.rotation, LayerMask.GetMask("Hitbox"));
+            foreach (Collider c in cols)
+            {
+                //check if the collider we hit is ourself 
+                if (c.transform.root == transform)
+                    continue;
+                //only print the collider from enemy  
+                Debug.Log(c.name);
+            }
+
+
         }
-
-
-    }
-}
-
-//child classes
-public class ArmAttack: Command
-{
-
-    public override void Execute(Collider armcol)
-    {
-        LaunchAttack(armcol);
-        //anim.SetBool("punch", true);
     }
 
-}
-
-public class ProjectileAttack:Command
-{
-    public override void Execute(Collider procol)
+    //child classes
+    public class ArmAttack : Command
     {
-        LaunchAttack(procol);
+
+        public override void Execute(Collider col, Transform transform)
+        {
+            LaunchAttack(col,transform);
+            //anim.SetBool("punch", true);
+
+       
+        }
+  
     }
-}
 
-public class DoNothing:Command
-{
-    public override void Execute(Collider col)
+    public class ProjectileAttack : Command
     {
+        public override void Execute(Collider col, Transform transform)
+        {
+            LaunchAttack(col,transform);
+        }
+    }
 
+    public class DoNothing : Command
+    {
+        public override void Execute(Collider col, Transform transform)
+        {
+
+        }
     }
 }
--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -2,80 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fighter : MonoBehaviour
+namespace CommandPatternNew
 {
-    private CharacterController controller;
-    private Vector3 moveVector;
-    private float verticalVelocity;
-    public Collider[] attackHitboxes;
-    Animator anim;
-    Command buttonG;
-    Command buttonH;
-    Command buttonJ;
-    // Start is called before the first frame update
-    void Start()
+    public class Fighter : MonoBehaviour
     {
-        controller = GetComponent<CharacterController>();
-        anim = GetComponent<Animator>();
-        buttonG = new ProjectileAttack();
-        buttonH = new ArmAttack();
-        buttonJ = new ArmAttack();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        HandleInput();
-        HandleMovement();
-    }
-    public void HandleInput()
-    {
-
-        //projectile
-        if (Input.GetKeyDown(KeyCode.G))
+        private CharacterController controller;
+        private Vector3 moveVector;
+        private float verticalVelocity;
+        public Collider[] attackHitboxes;
+        Animator anim;
+        Command buttonG;
+        Command buttonH;
+        // Start is called before the first frame update
+        void Start()
         {
-            buttonG.Execute(attackHitboxes[0]);
-     
-        }
-        //arm1
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            buttonH.Execute(attackHitboxes[1]);
-            anim.SetBool("punch", true);
-        }
-        //arm2
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            buttonJ.Execute(attackHitboxes[2]);
-            anim.SetBool("punch", true);
+            controller = GetComponent<CharacterController>();
+            anim = GetComponent<Animator>();
+            buttonG = new ProjectileAttack();
+            //right arm
+            buttonH = new ArmAttack();
         }
 
-    }
-    public void HandleMovement()
-    {
-
-        //handle jump 
-        if (controller.isGrounded)
+        // Update is called once per frame
+        void Update()
         {
-            Debug.Log("on the ground");
-            verticalVelocity = 0;
-            if (Input.GetKeyDown(KeyCode.Space))
+            HandleInput();
+            HandleMovement();
+        }
+        public void HandleInput()
+        {
+
+            //projectile
+            if (Input.GetKeyDown(KeyCode.G))
             {
-                verticalVelocity = 10;
+                buttonG.Execute(attackHitboxes[0],transform);
+
             }
+            //right arm
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                anim.SetBool("punch", true);
+                buttonH.Execute(attackHitboxes[1],transform);
+              
+            }
+
         }
-        else
-        { //the player is in the air, apply gravity
-            verticalVelocity -= 14 * Time.deltaTime;
+        public void HandleMovement()
+        {
+
+            //handle jump 
+            if (controller.isGrounded)
+            {
+                Debug.Log("on the ground");
+                verticalVelocity = 0;
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    verticalVelocity = 10;
+                }
+            }
+            else
+            { //the player is in the air, apply gravity
+                verticalVelocity -= 14 * Time.deltaTime;
+            }
+            moveVector = Vector3.zero;
+            moveVector.x = Input.GetAxis("Horizontal") * 5;
+            moveVector.y = verticalVelocity;
+            controller.Move(moveVector * Time.deltaTime);
+
         }
-        moveVector = Vector3.zero;
-        moveVector.x = Input.GetAxis("Horizontal") * 5;
-        moveVector.y = verticalVelocity;
-        controller.Move(moveVector * Time.deltaTime);
+
+
 
     }
-
-
-
 }
