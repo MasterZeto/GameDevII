729b6e820b29a030c57f6ea303982cd67867ee8c
Lorelei Wright <wrighl@rpi.edu>
2019-02-13 13:24:12 -0500

    Added camera controller so that the health system doesn't explode

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform follow;
+    [SerializeField] float lookAtHeight;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - follow.position;
+    }
+
+    void Update()
+    {
+        transform.position = follow.position + (follow.forward * offset.z)
+             + (Vector3.up * offset.y);
+        transform.LookAt(follow.position + (lookAtHeight * Vector3.up), Vector3.up);
+    }
+}
