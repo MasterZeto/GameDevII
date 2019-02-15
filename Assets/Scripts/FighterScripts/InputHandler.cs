d2b1fa43d5b7649c1e4d99d928138fd2e0c41fc3
Lorelei Wright <wrighl@rpi.edu>
2019-02-15 13:00:40 -0500

    Updated input handler to trigger dash actions on key press
    Still need to work on getting the dash to work on controller with the axies though.

--- Assets/Scripts/FighterScripts/InputHandler.cs
+++ Assets/Scripts/FighterScripts/InputHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    [SerializeField] float dash_window;
+
     FighterController fighter;
 
     float h, v;
@@ -18,6 +20,16 @@ public class InputHandler : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
+        CheckDoubleTap();
+
         fighter.Move((fighter.transform.right * h) + (fighter.transform.forward * v));
+    }
+
+    void CheckDoubleTap()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) fighter.DashLeft();
+        if (Input.GetKeyDown(KeyCode.H)) fighter.DashRight();
+        if (Input.GetKeyDown(KeyCode.T)) fighter.DashForward();
+        if (Input.GetKeyDown(KeyCode.G)) fighter.DashBackward();
     }
 }
