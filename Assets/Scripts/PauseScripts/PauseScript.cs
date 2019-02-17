dc0fd1ac161f38575150e72a9554c14f5009100b
Unknown <richas3@rpi.edu>
2019-02-17 14:09:30 -0500

    added a pause queue

--- Assets/Scripts/PauseScripts/PauseScript.cs
+++ Assets/Scripts/PauseScripts/PauseScript.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PauseScript : MonoBehaviour
 {
     //main script dealing w/ the pause stuff
+    //replace fightercontroller here for w/e is responsible for pausing the enemy
+    [SerializeField] FighterController enemy;
+    private List<voidDelegate> pauseQueue;
     FighterController playerActions;
     bool pause = false;
+    bool executing = false;
     // Start is called before the first frame update
     void Start()
     {
+        pauseQueue = new List<voidDelegate>();
+        pauseQueue.Add(() => playerActions.DashLeft());
+        pauseQueue.Add(() => playerActions.LeftPunch());
         playerActions = GameObject.FindWithTag("Player").GetComponent<FighterController>();
     }
 
@@ -17,13 +24,32 @@ public class PauseScript : MonoBehaviour
     void Update()
     {
         //move this to input handler later
-        if(Input.GetAxisRaw("Pause")==1&&!pause){
+        if(Input.GetAxisRaw("Pause") == 1){
+            if(!pause&&!executing){
             Time.timeScale = 0;
-            pause = true;   
-        }
-        else if(Input.GetAxisRaw("Pause")==1&&pause){
-            Time.timeScale = 1;
-            pause = false;
+            pause = true;
+            if(enemy!=null){
+                //stop enemy's action, somehow.
+            }   
+            }
+            else if(pause&&!executing){
+                Time.timeScale = 1;
+                executing = true;
+                pause = false;
+                ExecuteMoves();
+            }
         }
     }
+    void ExecuteMoves(){
+        Debug.Log("sdkfhsdkfh");
+        foreach(voidDelegate action in pauseQueue){
+            action();
+        }
+        executing = false;
+        if(enemy!=null){
+            //resume enemy's action, somehow
+        }
+    }
+    //used for the queue :/
+    public delegate void voidDelegate();
 }
