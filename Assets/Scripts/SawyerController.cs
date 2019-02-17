1e00a62d2bb8bee8d50a4fa8fbce60bc5fe954be
Ruofu <ruofu.sun@gmail.com>
2019-02-17 15:32:08 -0500

    base work for sawyer zigzag movement

--- Assets/Scripts/SawyerController.cs
+++ Assets/Scripts/SawyerController.cs
@@ -28,20 +28,55 @@ public class SawyerCharacter : AICharacter
         }
     }
 
-    
+
 }
 
 public class SawyerFSM : FiniteStateMachine<SawyerCharacter>
 {
-    private class TowardPlayer : MachineState<SawyerCharacter>
+    private class FirstZigZag : MachineState<SawyerCharacter>
     {
-        public TowardPlayer() { name = "TowardPlayer"; }
+        public FirstZigZag() { name = "FirstZigZag"; }
+
+        public override void Update(SawyerCharacter actor, float dt)
+        {  //if the direction is relative to AI, can not use update, maybe coroutine?
+            //if the direction is relative to the world, can use update
+            float P = Random.Range(0, 1);
+            float speed = 2.0f;
+            //find the vector that is perpendicular to the transform.forward vector on the xz plane
+            Vector3 tmp = Vector3.Cross(actor.character.transform.up, actor.character.transform.forward).normalized;
+            Vector3 moveDirection =Vector3.Lerp(tmp, actor.character.transform.forward, P);
+            //Vector3 moveDirection = Vector3.Lerp(actor.character.transform.right, actor.character.transform.forward, P);
+            actor.character.Move(moveDirection*speed);
+            Debug.Log("zig1"); 
+        }
+    }
+    private class SecondZigZag : MachineState<SawyerCharacter>
+    {
+        public SecondZigZag() { name = "SecondZigZag"; }
+
+        public override void Update(SawyerCharacter actor, float dt)
+        {  //if the direction is relative to AI, can not use update, maybe coroutine?
+            //if the direction is relative to the world, can use update
+            float P = Random.Range(0, 1);
+            float speed = 2.0f;
+            Vector3 moveDirection = Vector3.Lerp(new Vector3(0, 0, 1), actor.character.transform.forward, P);
+            //Vector3 moveDirection = Vector3.Lerp(actor.character.transform.right, actor.character.transform.forward, P);
+            actor.character.Move(moveDirection * speed);
+            Debug.Log("zig2");
+        }
+    }
+    private class DashToPlayer : MachineState<SawyerCharacter>
+    {
+        public  DashToPlayer () { name = "DashToPlayer"; }
 
         public override void Update(SawyerCharacter actor, float dt)
         {
-            actor.character.SimpleMove(actor.character.transform.forward * 3f);            
+            actor.character.SimpleMove(actor.character.transform.forward * 10f);
+            Debug.Log("DashToPlayer");
         }
     }
+
+
 
     private class AttackPlayer : MachineState<SawyerCharacter>
     {
@@ -50,6 +85,7 @@ public class SawyerFSM : FiniteStateMachine<SawyerCharacter>
         public override void Update(SawyerCharacter actor, float dt)
         {
             actor.Attack();
+            Debug.Log("should attack now");
         }
     }
 
@@ -61,18 +97,18 @@ public class SawyerFSM : FiniteStateMachine<SawyerCharacter>
         float dist_to_player = Vector3.Distance(actor.character.transform.position, Blackboard.player_position);
         switch(state.name)
         {
-            case "TowardPlayer": 
-                if (dist_to_player < 2f) 
+            case "FirstZigZag": 
+                if (dist_to_player < 2f&& dist_to_player>1f) 
                 { 
                     actor.animator.SetBool("walk", false);
-                    return new AttackPlayer(); 
+                    return new SecondZigZag(); 
                 }
                 break;
-            case "AttackPlayer": 
-                if (dist_to_player > 3f)
+            case "SecondZigZag": 
+                if (dist_to_player < 1f)
                 {
                     actor.animator.SetBool("walk", true);
-                    return new TowardPlayer();
+                    return new DashToPlayer();
                 }
                 break;
         }
@@ -82,7 +118,7 @@ public class SawyerFSM : FiniteStateMachine<SawyerCharacter>
     SawyerCharacter actor;
 
     public SawyerFSM(SawyerCharacter actor) 
-         : base(new TowardPlayer(), next_state) 
+         : base(new FirstZigZag(), next_state) 
     {
         this.actor = actor;
     }
@@ -110,5 +146,6 @@ public class SawyerController : MonoBehaviour
         ).normalized;
         fsm.Update(ai, Time.deltaTime);
     }
+
 
 }
