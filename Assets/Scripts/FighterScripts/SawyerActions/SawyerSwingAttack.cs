57e54fcebdf954e7cf7cde7d0c9f1b6025946dd3
Unknown <richas3@rpi.edu>
2019-02-26 19:47:00 -0500

    changed some casting stuff

--- Assets/Scripts/FighterScripts/SawyerActions/SawyerSwingAttack.cs
+++ Assets/Scripts/FighterScripts/SawyerActions/SawyerSwingAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SawyerSwingAttack : Action
 {
-    [SerializeField] Hitbox hitbox;
+    public Hitbox hitbox;
     [SerializeField] float hit_duration;
     [SerializeField] float hit_delay;
     [SerializeField] string anim_name;
