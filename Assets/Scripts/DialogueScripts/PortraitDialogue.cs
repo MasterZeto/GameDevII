c06a4a006766cdacd056efd01e9f95d99a27917c
Lorelei Wright <wrighl@rpi.edu>
2019-03-11 23:55:07 -0400

    Early dialogue system stuff added

---
+++ Assets/Scripts/DialogueScripts/PortraitDialogue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitDialogue : MonoBehaviour
+{
+    [System.Serializable]
+    public struct PortraitDialogueBlock
+    {
+        public int index;
+        public List<string> dialogue;
+    }
+
+    [SerializeField] List<PortraitDialogueBlock> text;
+    [SerializeField] List<Typewriter> typewriters;
+
+    bool printing = false;
+    bool next = false;
+    int index = 0;
+
+    [ContextMenu("Start Dialogue")]
+    void StartDialogue()
+    {
+        printing = true;
+        next = false;
+        index = 0;
+
+        DisplayBlock(index);
+    }
+
+    void Update()
+    {
+        if (printing && next)
+        {
+            next = false;
+            DisplayBlock(index);
+        }
+    }
+
+    void DisplayBlock(int i)
+    {
+        typewriters[text[i].index].StartTypewriter(
+            text[i].dialogue, 
+            "", 
+            null, 
+            () => { IncrementDialogueBlock(); }
+        );
+    }
+
+    void IncrementDialogueBlock()
+    {
+        index++;
+        if (index >= text.Count)
+        {
+            next = false;
+            printing = false;
+        }
+        else
+        {
+            next = true;
+        }
+    }
+    
+}
