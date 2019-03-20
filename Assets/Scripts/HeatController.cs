1ca617d1f2757889b1cdf651a557a9cc29ea05d1
Troy Mundschenk <tbobmundschenk@gmail.com>
2019-03-20 17:47:20 -0400

    Hourglass functions - UseHeat(float amount)

---
+++ Assets/Scripts/HeatController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeatController : MonoBehaviour
+{
+	public GameObject topHeatMask;
+	public GameObject bottomHeatMask;
+    
+	private RectTransform topMaskSize;
+	private RectTransform bottomMaskSize;
+    void Start()
+    {
+        topMaskSize = topHeatMask.GetComponent<RectTransform>();
+        bottomMaskSize = bottomHeatMask.GetComponent<RectTransform>();
+    }
+
+    public void Update() 
+    {
+    	if(topMaskSize.sizeDelta.y < 40)
+    	{
+			topMaskSize.sizeDelta = new Vector2(topMaskSize.sizeDelta.x, topMaskSize.sizeDelta.y + .1f);
+		}
+		if(bottomMaskSize.sizeDelta.y > .1)
+		{
+			bottomMaskSize.sizeDelta = new Vector2(bottomMaskSize.sizeDelta.x, bottomMaskSize.sizeDelta.y - .1f);
+		}
+    }
+
+    public void UseHeat(float amount)
+    {
+    	topMaskSize.sizeDelta = new Vector2(topMaskSize.sizeDelta.x, topMaskSize.sizeDelta.y - ((amount / 100f) * 40f));
+    	bottomMaskSize.sizeDelta = new Vector2(bottomMaskSize.sizeDelta.x, bottomMaskSize.sizeDelta.y - ((amount / 100f) * 40f));
+    }
+ 
+}
