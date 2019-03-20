308cf20b6ac45ead97c5c71912890cfb37f50f01
Unknown <richas3@rpi.edu>
2019-03-20 15:16:48 -0400

    Added Baha gloat

---
+++ Assets/Scripts/FighterScripts/BahaActions/GloatAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloatAction : Action
+{
+    [SerializeField] float gloat_time = 1f;
+    [SerializeField] string anim_name;
+    bool paused;
+
+
+    public override void StartAction(FighterController fighter){
+        this.fighter = fighter;
+        running = true;
+        paused = false;
+        if(anim_name!="") fighter.SetTrigger(anim_name);
+        StartCoroutine(Gloat());
+
+    }
+    public override void Stop() {running = false; }
+    public override void Pause(){ paused = true; }
+    public override void Resume(){ paused = false; }
+    private IEnumerator Gloat(){
+        for(float t = 0f; t < gloat_time; t+=Time.deltaTime){
+            while(paused){
+                yield return null;
+            }
+        }
+        running = false;
+    }
+}
