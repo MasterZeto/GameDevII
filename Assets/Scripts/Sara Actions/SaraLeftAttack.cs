6cbe97a4b8fe029add59400319d01725911247a1
Ruofu <ruofu.sun@gmail.com>
2019-03-21 17:39:35 -0400

    Randomly instantiate one of the four gems during both left attack and right attack for Sara

--- Assets/Scripts/Sara Actions/SaraLeftAttack.cs
+++ Assets/Scripts/Sara Actions/SaraLeftAttack.cs
@@ -7,13 +7,18 @@ public class SaraLeftAttack : Action
     public Hitbox hitbox;
     [SerializeField] float hit_duration;
     [SerializeField] string anim_name;
+    [SerializeField] GameObject[] gems = new GameObject[4];
+    [SerializeField] Transform LeftCannon;
 
     public override void StartAction(FighterController fighter)
     {
         this.fighter = fighter;
         hitbox.Fire(hit_duration);
         fighter.SetTrigger(anim_name);
-  //      transform.gameObject.GetComponent<SoundBox>().MissSFX();
+        int rand = Random.Range(0, 4);
+        Instantiate(gems[rand],LeftCannon.position,Quaternion.identity);
+
+        //      transform.gameObject.GetComponent<SoundBox>().MissSFX();
     }
 
     public override void Stop() {}
