f5efff33cc979c8da1c39e770bfa7eef7d2f9917
Ruofu <ruofu.sun@gmail.com>
2019-03-27 15:49:56 -0400

    Instantiate three new diamonds at certain distance

---
+++ Assets/Scripts/Gems for Sara/DiamondChild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondChild : MonoBehaviour
+{
+    GameObject player;
+    float speed = 17f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(player.transform);
+    }
+
+ 
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
--- Assets/Scripts/Gems for Sara/DiamondMoveTrack.cs
+++ Assets/Scripts/Gems for Sara/DiamondMoveTrack.cs
@@ -7,6 +7,7 @@ public class DiamondMoveTrack : MonoBehaviour
     GameObject player;
     float speed = 17f;
     float distance;
+    GameObject child;
     GameObject child1;
     GameObject child2;
     GameObject child3;
@@ -15,16 +16,19 @@ public class DiamondMoveTrack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        child = Resources.Load("DiamondChild") as GameObject;
         Debug.Log("diamond");
         player = GameObject.FindGameObjectWithTag("Player");
         transform.LookAt(player.transform);
-        distance = Vector3.Distance( player.transform.position , transform.position);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(distance);
+        //Debug.Log(distance);
+        distance = Vector3.Distance(player.transform.position, transform.position);
         transform.position += transform.forward * speed * Time.deltaTime;
         if (distance < 20f && canReborn)
         {
@@ -38,15 +42,17 @@ public class DiamondMoveTrack : MonoBehaviour
 
 
     void Reborn() {
-       //face the same direction toward player
-       child1= Instantiate(gameObject, transform.position, Quaternion.identity);
+        //face the same direction toward player
+      
+        child1= Instantiate(child, transform.position, Quaternion.identity);
         //face to the left of player
-       child2 = Instantiate(gameObject, transform.position, Quaternion.identity);
-       // child2.transform.eulerAngles +=new Vector3(0, 0, 45);
+        child2 = Instantiate(child, transform.position, Quaternion.identity);
+        child2.transform.eulerAngles +=new Vector3(0, 0, 45);
 
         //face to the right of player
-       child3 = Instantiate(gameObject, transform.position, Quaternion.identity);
-       // child3.transform.eulerAngles += new Vector3(0, 0, -45);
+       child3 = Instantiate(child, transform.position, Quaternion.identity);
+        child3.transform.eulerAngles += new Vector3(0, 0, -45);
+     
         Destroy(gameObject);
 
 
