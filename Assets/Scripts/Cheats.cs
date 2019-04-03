e437cb964ea6afff2b55fb182c37e36843ff77ac
Lorelei Wright <wrighl@rpi.edu>
2019-04-03 16:25:29 -0400

    Cheats (1-8 to switch scenes)

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cheats : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { SceneManager.LoadScene("MainMenu"); }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { SceneManager.LoadScene("SawyerBracketScene"); }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { SceneManager.LoadScene("SawyerFight"); }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) { SceneManager.LoadScene("SaraBracketScene"); }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) { SceneManager.LoadScene("SaraOHara"); }
+        if (Input.GetKeyDown(KeyCode.Alpha6)) { SceneManager.LoadScene("BajaBracketScene"); }
+        if (Input.GetKeyDown(KeyCode.Alpha7)) { SceneManager.LoadScene("BahaTest"); }
+        if (Input.GetKeyDown(KeyCode.Alpha8)) { SceneManager.LoadScene("EndCutscene"); }      
+    }
+}
