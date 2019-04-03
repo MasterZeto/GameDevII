a6572b6a2118c74e07c00f46487cc1cbe658ba3d
Lorelei Wright <wrighl@rpi.edu>
2019-04-03 09:22:25 -0400

    Added end credits scene, removed options option

---
+++ Assets/Scripts/EndCutscenePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCutscenePlayer : MonoBehaviour
+{
+    [SerializeField] PortraitDialogue dialogue;
+    [SerializeField] SceneTransition transition;
+    [SerializeField] SceneFade fade;
+
+    void Start()
+    {
+        StartCoroutine(ExecAfterDelay(fade.FadeIn, 0.01f));
+        StartCoroutine(ExecAfterDelay(PlayDialogue, 1.5f));
+    }
+
+    void PlayDialogue()
+    {
+        dialogue.StartDialogue(transition.Transition);
+    }
+
+    private IEnumerator ExecAfterDelay(VoidDelegate func, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        func();
+    }
+}
