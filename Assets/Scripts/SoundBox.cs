04f8ef4a5c0f39dd7f531e19a7feb64501650356
Troy Mundschenk <tbobmundschenk@gmail.com>
2019-04-16 23:56:31 -0400

    Slowed combat theme in pause

--- Assets/Scripts/SoundBox.cs
+++ Assets/Scripts/SoundBox.cs
@@ -5,41 +5,46 @@ using UnityEngine;
 public class SoundBox : MonoBehaviour
 {
 
-	public AudioSource source;
+	public AudioSource playerSource;
     public AudioLowPassFilter muffle;
     public AudioClip hitSound;
     public AudioClip missSound;
     public AudioClip slowSound;
+
+    public AudioSource musicSource;
     public AudioLowPassFilter low_pass;
 
     GameObject speaker;
 
     void Awake()
     {
-        source = GetComponent<AudioSource>();
+        playerSource = GetComponent<AudioSource>();
+        musicSource = GameObject.Find("Center Light").GetComponent<AudioSource>();
         low_pass = GameObject.Find("Center Light").GetComponent<AudioLowPassFilter>();
     }
 
 
     public void HitSFX()
 	{
-     	source.PlayOneShot(hitSound, 0.3F);
+     	playerSource.PlayOneShot(hitSound, 0.3F);
     }
 
     public void MissSFX()
     {
-        source.PlayOneShot(missSound, 0.3F);
+        playerSource.PlayOneShot(missSound, 0.3F);
     }
 
     public void TimeSlowSFX()
     {
-        source.PlayOneShot(slowSound, 0.3F);
+        playerSource.PlayOneShot(slowSound, 0.3F);
         low_pass.enabled = true;    
+        musicSource.pitch = 0.5F;
     }
 
     public void TimeSlowStop()
     {
-        low_pass.enabled = false;    
+        low_pass.enabled = false;   
+        musicSource.pitch = 1.0F; 
     }
 
   
