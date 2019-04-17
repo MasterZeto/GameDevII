88cec997c1817329cbce18b5522e3828aa3b1742
Troy Mundschenk <tbobmundschenk@gmail.com>
2019-04-17 11:21:37 -0400

    Menu sounds, waiting to implement until MainMenu is available

---
+++ Assets/ButtonSounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour
+{
+	public AudioSource beepSource;
+	public AudioClip beep;
+
+    void playSound() 
+    {
+    	beepSource.PlayOneShot(beep);
+    }
+}
